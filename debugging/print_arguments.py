178f08979f48ab7549d5fd73af84684ffcfb8811
phon xay <majic42@hotmail.com>
2024-09-03 18:00:44 +1000

    Update print_arguments.py

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 import sys
-import os
 
-try:
-    print(os.path.basename(sys.argv[0]))
-    for i in range(1, len(sys.argv)):
-        print(sys.argv[i])
-except Exception as e:
-    print(f"Error: {e}")
-    sys.exit(1)
+for i in range(len(sys.argv)):
+    print(sys.argv[i])
